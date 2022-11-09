4fa117168188eb0d4e0a2eb6c514a806535aa7d2
Genialngash <ngangadanche@gmail.com>
2022-11-09 17:39:20 +0300

    api urls

--- lib/services/api_urls.dart
+++ lib/services/api_urls.dart
@@ -1,4 +1,12 @@
-class ApiUrl {
-  static String BASE_URL =
+class ApiUrls {
+  static String BASE_URL_TESTING =
       "https://sandbox.sasapay.me/apps/lipa-rahisi/api/v1/";
+  //TODO change to the correct url
+  static String BASE_URL_PRODUCTION =
+      "https://sandbox.sasapay.me/apps/lipa-rahisi/api/v1/";
+
+  static String REGISTER_CONFIRMATION_URL = "payments/register-ipn-url/";
+
+  //TODO UPCOMING FEATURE
+  static String REGISTER_VALIDATION_URL = "payments/register-ipn-url/";
 }
