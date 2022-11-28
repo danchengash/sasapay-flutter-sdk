7f00a6c2a320f99e2a722ae1d738136e3fdfa515
Genialngash <ngangadanche@gmail.com>
2022-11-28 13:02:24 +0300

    export main sdk class

--- lib/sasapay_sdk.dart
+++ lib/sasapay_sdk.dart
@@ -1,7 +1,3 @@
 library sasapay_sdk;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:sasapay_sdk/initialize_sdk.dart';
