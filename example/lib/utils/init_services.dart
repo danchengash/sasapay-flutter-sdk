9417c54817e2fc00caeb2cc1d41a0eee92cc8f3a
Genialngash <ngangadanche@gmail.com>
2022-12-06 15:09:22 +0300

    EnvironmentSasaPay.Testing,

--- example/lib/utils/init_services.dart
+++ example/lib/utils/init_services.dart
@@ -1,7 +1,7 @@
 import 'package:get/instance_manager.dart';
 import 'package:sasapay_sdk/helper_functions.dart';
 import 'package:sasapay_sdk/initialize_sdk.dart';
-import 'package:sasapay_sdk/utils/helper_enums.dart';
+import 'package:sasapay_sdk/utils/helper_enums_consts.dart';
 
 sasaPayServicesInit() {
   // initialize sasapay class
@@ -10,7 +10,7 @@ sasaPayServicesInit() {
       clientId: "8mgx3sf4QhfZpN7aG9DIVdrrMVyTFxU89gz5gaur",
       clientSecret:
           "EWbIcQEhd3acV8vcAAyuldKpp2EaWNpda4GfQHuANW5biExHDLcGLuxJ6BV1UgHNODfXUUsQqwHBSlc9KINFofXQjQ7DuqI124aICYjsz5MiGn5KajTA8F1YbOQMhHtM",
-      environment: Environment.Testing,
+      environment: EnvironmentSasaPay.Testing,
     ),
   );
 }
