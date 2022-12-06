be1a1c079fa8842b6bb0078b6ab4018140269e9c
Genialngash <ngangadanche@gmail.com>
2022-12-06 18:48:32 +0300

    model update

--- lib/models/bank_model.dart
+++ lib/models/bank_model.dart
@@ -1,38 +1,6 @@
-// To parse this JSON data, do
-//
-//     final bankModel = bankModelFromJson(jsonString);
+
 
 import 'dart:convert';
-
-BankModel bankModelFromJson(String str) => BankModel.fromJson(json.decode(str));
-
-String bankModelToJson(BankModel data) => json.encode(data.toJson());
-
-class BankModel {
-  BankModel({
-    required this.status,
-    required this.message,
-    required this.channelCodes,
-  });
-
-  bool status;
-  String message;
-  List<BanksChannelCode> channelCodes;
-
-  factory BankModel.fromJson(Map<String, dynamic> json) => BankModel(
-        status: json["status"],
-        message: json["message"],
-        channelCodes: List<BanksChannelCode>.from(
-            json["channel_codes"].map((x) => BanksChannelCode.fromJson(x))),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "status": status,
-        "message": message,
-        "channel_codes":
-            List<dynamic>.from(channelCodes.map((x) => x.toJson())),
-      };
-}
 
 class BanksChannelCode {
   BanksChannelCode({
