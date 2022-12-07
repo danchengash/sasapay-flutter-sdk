12c0ad3db17c92853e7cedcfd45120dd87113abe
Genialngash <ngangadanche@gmail.com>
2022-12-07 18:19:24 +0300

    app bar themes

--- example/lib/helpers/themes_colors.dart
+++ example/lib/helpers/themes_colors.dart
@@ -4,4 +4,5 @@ class CustomColor {
   static Color blueColor = Color(0xff004D85);
   static Color getdarkcolor = Colors.black;
   static Color getdarkwhitecolor = Colors.white12;
+  static Color appBarColor = Color(0xFF7F011A);
 }
