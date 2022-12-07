be2dbaab227109960960eb1a234579cc0e72df73
Genialngash <ngangadanche@gmail.com>
2022-12-07 17:34:46 +0300

    posthere.io call back url

--- example/lib/utils/utils_helpers.dart
+++ example/lib/utils/utils_helpers.dart
@@ -1,3 +1,3 @@
 //TODO update merchande code
 const String MERCHANT_CODE = "600980";
-const String CALL_BACK_URL = "https://8889-41-90-115-26.eu.ngrok.io";
+const String CALL_BACK_URL = "https://posthere.io/1364-4468-97f2";
